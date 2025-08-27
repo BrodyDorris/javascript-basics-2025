ba82fb4cc9029fed1762c415c9cdd2234450d64b
Brody Dorris <Dorrisamanda@gmail.com>
2025-08-27 11:06:15 -0600

    added things

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
 // This will be our first javascript file
+
+console.log ("greetings world, sincerely: Javascript");
+
+//this is a string varaiable
+//strings are surrounded by "", '', or ``.
+
+let username = "Brody";
+let favSong = "";
+
+console.log("my name is", username);
+
+favSong = "";
