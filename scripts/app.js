40ad436b95a32f28c5606ef20d30a0dd6ab5affc
Brody Dorris <Dorrisamanda@gmail.com>
2025-08-29 16:03:39 -0600

    Had to push up code

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,30 @@
 // This will be our first javascript file
 
-console.log ("greetings world, sincerely: Javascript");
+console.log("greetings world, sincerely: Javascript");
 
 //this is a string varaiable
 //strings are surrounded by "", '', or ``.
 
 let username = "Brody";
-let favSong = "";
+let favSong = "lkjhgfdsx";
 
 console.log("my name is", username);
 
-favSong = "";
+favSong = "lkjhgfdiut";
+
+let whatHappens = username + favSong + 1000;
+
+console.log(whatHappens);
+
+let array = ["three", "one", "four", "one", "five", "nine"];
+console.log(array, array[5]);
+
+let profile = {
+	username: username,
+	favGame: "Chess",
+	likes: 0,
+	subscribers: 0,
+	friends: ["Brody", "..."]
+};
+
+console.log(profile);
