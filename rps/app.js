f2588098fc6d3afb5dedf76fcd8a2360da2b9ec6
Brody Dorris <Dorrisamanda@gmail.com>
2025-09-11 16:06:06 -0600

    show rps messages

--- rps/app.js
+++ rps/app.js
@@ -3,21 +3,38 @@
 const STONE = "stone";
 const PARCHMENT = "parchment";
 const SHEARS = "shears";
-var win = 0;
 
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below
 let resultsElement = document.getElementById("game-results");
+if (resultsElement == null) {
+	throw "aftermath is not defined";
+}
 
-const pickWeapon = function(weapon) {
+const pickWeapon = function (weapon) {
+	let aftermathText = `The player elected to choose ${weapon}. `;
 	console.log("The contender elected to choose", weapon);
-	selectComputerWeapon();
-	determineOutcome();
-	
+
 	let computerWeapon = selectComputerWeapon();
-	console.log(computerWeapon);
+	console.log("the computer elected to choose", computerWeapon);
+
+	let results = determineOutcome(weapon, computerWeapon);
+	console.log("outcome: ", results);
+
+	let winner = "";
+	if (results?.isDraw) {
+		winner = results.description;
+	} else if (results?.playerVictorious) {
+		winner = "The player prevails";
+	} else {
+		winner = "The computer prevails";
+	}
+	aftermathText += `${winner} as ${results?.description}.`;
+
+	resultsElement.textContent = aftermathText;
 };
 
-
-const selectComputerWeapon = function() {
+const selectComputerWeapon = function () {
 	const rand = Math.floor(Math.random() * 3);
 
 	if (rand == 0) {
@@ -46,37 +63,43 @@ function determineOutcome(playerWeapon, computerWeapon) {
 
 	if (playerWeapon == STONE && computerWeapon == SHEARS) {
 		outcome.playerVictorious = true;
-		outcome.description = "Stone is victorious, Shears have been demolished.";
+		outcome.description =
+			"Shears do not triumph; Stone supersedes Shears in this contest.";
 		return outcome;
 	}
 
 	if (playerWeapon == STONE && computerWeapon == PARCHMENT) {
 		outcome.playerVictorious = false;
-		outcome.description = "Stone is not victorious, Stone has been demolished.";
+		outcome.description =
+			"Stone does not triumph; Parchment supersedes Stone in this contest.";
 		return outcome;
 	}
 
 	if (playerWeapon == PARCHMENT && computerWeapon == SHEARS) {
 		outcome.playerVictorious = false;
-		outcome.description = "Parchment is not victorious, Parchment has been demolished.";
+		outcome.description =
+			"Parchment does not triumph; Shears supersedes Parchment in this contest.";
 		return outcome;
 	}
 
 	if (playerWeapon == PARCHMENT && computerWeapon == STONE) {
 		outcome.playerVictorious = true;
-		outcome.description = "Parchment is victorious, Stone has been demolished.";
+		outcome.description =
+			"Stone does not triumph; Parchment supersedes Stone in this contest.";
 		return outcome;
 	}
 
 	if (playerWeapon == SHEARS && computerWeapon == PARCHMENT) {
 		outcome.playerVictorious = true;
-		outcome.description = "Shears are victorious, Parchment has been demolished.";
+		outcome.description =
+			"Parchment not triumph; Shears supersedes Parchment in this contest.";
 		return outcome;
 	}
 
 	if (playerWeapon == SHEARS && computerWeapon == STONE) {
 		outcome.playerVictorious = false;
-		outcome.description = "Shears are not victorious, Shears has been demolished.";
+		outcome.description =
+			"Shears do not triumph; Stone supersedes Shears in this contest.";
 		return outcome;
 	}
 
